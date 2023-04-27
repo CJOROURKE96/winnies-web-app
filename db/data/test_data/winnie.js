92a0ce55714b9c69e84c1b262899a00fcdfd6226
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-04-27 18:36:08 +0100

    add new image of winnie

--- db/data/test_data/winnie.js
+++ db/data/test_data/winnie.js
@@ -2,8 +2,7 @@ module.exports = [
   {
     name: 'Winnie',
     location: 'Bamber Bridge',
-    image:
-      'https://scontent.fman4-2.fna.fbcdn.net/v/t31.18172-8/18518335_101540550422523_9127068466022832608_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=4nkbp-bcre4AX99hUje&_nc_ht=scontent.fman4-2.fna&oh=00_AfAZyNYx9iZJXG0AXSiqghX4qDXouOICA2F56UGXeJ15Dw&oe=64721FA3',
+    image: 'https://i.imgur.com/9QJr1jh.png',
   },
   {
     name: 'Doge',

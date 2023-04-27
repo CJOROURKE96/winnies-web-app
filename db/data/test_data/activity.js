9b17fa6bdb62c7280185c7eda3a55bd45ecaa90f
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-04-27 20:24:34 +0100

    change test db image source

--- db/data/test_data/activity.js
+++ db/data/test_data/activity.js
@@ -2,8 +2,7 @@ module.exports = [
   {
     location: 'Bamber Bridge',
     activity: 'Relaxing',
-    image:
-      'https://scontent.fman4-2.fna.fbcdn.net/v/t31.18172-8/18518335_101540550422523_9127068466022832608_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=4nkbp-bcre4AX99hUje&_nc_ht=scontent.fman4-2.fna&oh=00_AfAZyNYx9iZJXG0AXSiqghX4qDXouOICA2F56UGXeJ15Dw&oe=64721FA3',
+    image: 'https://imgur.com/a/DYpB1OX',
   },
   {
     location: 'test',

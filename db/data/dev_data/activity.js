9fbf9e1bc3efa13bc3ccf500bc6d6374da447b39
Connor ORourke <orourkeconnor@hotmail.co.uk>
2023-04-27 16:42:14 +0100

    add data about winnie and her activities

--- db/data/dev_data/activity.js
+++ db/data/dev_data/activity.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    name: 'Winnie',
     location: 'Bamber Bridge',
+    activity: 'Relaxing',
     image:
       'https://scontent.fman4-2.fna.fbcdn.net/v/t31.18172-8/18518335_101540550422523_9127068466022832608_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=4nkbp-bcre4AX99hUje&_nc_ht=scontent.fman4-2.fna&oh=00_AfAZyNYx9iZJXG0AXSiqghX4qDXouOICA2F56UGXeJ15Dw&oe=64721FA3',
   },
---
+++ db/data/dev_data/index.js
@@ -0,0 +1,7 @@
+const winnieData = require('./winnie');
+const activityData = require('./activity');
+
+module.exports = {
+  winnieData,
+  activityData,
+};
